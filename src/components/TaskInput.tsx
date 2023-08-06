e022552b9e501aff72eed3be39240a1dfbf7e79d
Marina Portugal <marinapsvreis@gmail.com>
2023-08-06 18:16:21 -0300

    :lipstick: New task button adjust

--- src/components/TaskInput.tsx
+++ src/components/TaskInput.tsx
@@ -12,7 +12,7 @@ export default function TaskInput() {
 	return (
 		<>
 			<div className="flex gap-2">
-				<button onClick={handleOpenDialog} className="w-36 bg-blue-600 hover:bg-blue-700 transition text-[white] px-2 py-1 rounded-lg flex justify-center items-center gap-1" title="Add new task"><PlusCircle size={16} />New task</button>
+				<button onClick={handleOpenDialog} className="w-36 bg-blue-600 hover:bg-blue-700 transition text-[white] px-2 py-2 rounded-lg flex justify-center items-center gap-1" title="Add new task"><PlusCircle size={22} />New task</button>
 			</div>
 			<Dialog type={"task"} open={openDialog} setOpen={setOpenDialog}/>
 		</>	
