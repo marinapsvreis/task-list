c37c403accc86f47fc14876049e1cf451f63e750
Marina Portugal <marinapsvreis@gmail.com>
2023-08-06 18:14:56 -0300

    :lipstick: Adjust background header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Header() {
 	return (
-		<div className="flex flex-col items-center justify-center w-full pt-4 pb-12 -mb-5 bg-gradient-to-r from-sky-500 to-indigo-500">
+		<div className="flex flex-col items-center justify-center w-full pt-4 pb-12 -mb-5 bg-gradient-to-r from-blue-300 to-indigo-500">
 			<Image
 				src={logoImg}
 				width={50}
