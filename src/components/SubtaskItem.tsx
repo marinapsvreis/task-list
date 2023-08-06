d201d292f7d6a3d35cce20276e064fa307dee3f4
Marina Portugal <marinapsvreis@gmail.com>
2023-08-06 19:14:04 -0300

    :lipstick: More improvements to layout subtaks

--- src/components/SubtaskItem.tsx
+++ src/components/SubtaskItem.tsx
@@ -11,9 +11,11 @@ export default function SubtaskItem({ subtask }: SubtaskItemProps) {
 	const { updateSubtaskList, deleteSubstaskFromTask } = useContext(TasksContext);
   
 	return (
-		<div className="flex items-center gap-2 p-2 mt-2 ml-6 rounded-lg shadow bg-gray-500/10">
-			<input type="checkbox" checked={subtask.checked} onChange={() => updateSubtaskList(subtask)} className="rounded-full cursor-pointer form-checkbox focus:ring-white w-[20px] h-[20px]" />
-			<p className={`${subtask.checked && "line-through" }`}>{subtask.name}</p>
+		<div className="flex items-center justify-between gap-2 p-2 mt-2 ml-6 rounded-lg shadow bg-gray-500/10">
+			<div className="flex gap-2">
+				<input type="checkbox" checked={subtask.checked} onChange={() => updateSubtaskList(subtask)} className="rounded-full cursor-pointer form-checkbox focus:ring-white w-[20px] h-[20px]" />
+				<p className={`${subtask.checked && "line-through" }`}>{subtask.name}</p>
+			</div>
 			<button className="flex items-center justify-center pb-1 leading-3 " title="Delete" onClick={() => deleteSubstaskFromTask(subtask)}>
 				<Trash size={24} weight="fill" className="pt-1 text-gray-400 hover:text-red-600" />
 			</button>
