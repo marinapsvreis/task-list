0614499d23ac190128e6476faeb89ce04a5e6a61
Marina Portugal <marinapsvreis@gmail.com>
2023-08-08 18:04:48 -0300

    :bug: Test apit route in production bug

--- src/pages/api/tasks/create.ts
+++ src/pages/api/tasks/create.ts
@@ -5,7 +5,7 @@ export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse,
 ) {
-	if(req.method !== "POST") return res.status(405).end();
+	// if(req.method !== "POST") return res.status(405).end();
 
 	const bodySchema = z.object({
 		name: z.string().max(450)
