4dc9de7f94ca0232f44bb5af683683179e61c5eb
Marina Portugal <marinapsvreis@gmail.com>
2023-08-12 10:56:13 -0300

    :white_check_mark: Add more mocked unit tests

--- src/services/tasks.test.ts
+++ src/services/tasks.test.ts
@@ -1,5 +1,5 @@
 import { api } from "@/lib/axios";
-import { getAllTasks } from "./tasks";
+import { getAllTasks, getTaskById } from "./tasks";
 
 describe("getAllTasks", () => {
 	it("should return all tasks when API call is successful", async () => {
@@ -24,6 +24,51 @@ describe("getAllTasks", () => {
 		jest.spyOn(api, "get").mockResolvedValueOnce({ data: mockData, status: 200 });
 		const result = await getAllTasks();
 		expect(result).toEqual(mockData);
+	});
+
+	it("should return an empty array when there are no tasks", async () => {
+		jest.spyOn(api, "get").mockResolvedValueOnce({ data: { tasks: []}, status: 200 });
+		const result = await getAllTasks();
+		expect(result.tasks).toEqual([]);
+	});
+	
+	it("should throw an error when API call fails", async () => {
+		jest.spyOn(api, "get").mockRejectedValueOnce(new Error("API Error"));
+		await expect(getAllTasks()).rejects.toThrow("API Error");
+	});
+});
+
+
+
+
+describe("getTaskById", () => {
+	it("should return the correct task object when given a valid task ID", async () => {
+		const id = 1;
+		const expectedData = {
+			task: {
+				id: 1,
+				name: "Task 1",
+				checked: false,
+				createdAt: new Date(),
+				subtasks: [
+					{
+						id: 1,
+						name: "Test Subtask 1",
+						checked: false,
+						taskId: 1
+					},
+					{
+						id: 2,
+						name: "Test Subtask 2",
+						checked: false,
+						taskId: 1
+					}
+				]
+			}
+		};
+		jest.spyOn(api, "get").mockResolvedValueOnce({ data: expectedData, status: 200 });
+		const task = await getTaskById(id);
+		expect(task).toEqual(expectedData.task);
 	});
 });
 
